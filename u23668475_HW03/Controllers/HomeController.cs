570c9596433922af22938f001c94a274bb875858
Dylan Esterhuizen <u23668475@up.ac.za>
2024-10-22 18:26:14 +0200

    test

--- u23668475_HW03/Controllers/HomeController.cs
+++ u23668475_HW03/Controllers/HomeController.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Threading.Tasks;
 
 namespace u23668475_HW03.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            using (var dbContext = new LibraryEntities())
+            {
+                 var AuthorsList = await dbContext.authors.ToListAsync();
+                 var BooksList = await dbContext.books.ToListAsync();
+                 var BorrowsList = await dbContext.borrows.ToListAsync();
+                 var StudentsList = await dbContext.students.ToListAsync();
+                 var TypeList = await dbContext.types.ToListAsync();
+            }
+                return View();
         }
 
         public ActionResult About()
